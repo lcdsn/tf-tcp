d5ca6b03386df6d7df015a634df0d4fe9aa8167e
lucas-caique <lucascaique.nogueira@gmail.com>
2024-07-10 21:04:51 -0300

    move código de tradução para função separada

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ channel = 0
 time = 0
 duration = 1
 tempo = 60
-bpm = 60
+bpm = 120
 default_volume = 100
 volume = default_volume
 pitch = 0
@@ -42,9 +42,60 @@ def createMidi(notesList, output_file):
         midi.writeFile(output_file)
 
 
+def translateText2MIDI(text: str) -> list[tuple]:
+    notesList = []
+    prevChar = None
+    global bpm, instrument, octave, volume, duration, time
+
+    for i in range(len(text)):
+        ch = text[i].lower()
+        if not ch:
+            break
+        if ch in notes:
+            notesList.append(
+                (notes[ch] + 12 * octave, volume, duration, instrument, bpm)
+            )
+            prevChar = ch
+        elif ch in "oiu":
+            if prevChar in notes:
+                notesList.append(
+                    (notes[prevChar] + 12 * octave, volume, duration, instrument, bpm)
+                )
+                prevChar = ch
+            else:
+                notesList.append((notes["c"] + 12 * octave, volume, duration, 124, bpm))
+        elif ch == " ":
+            notesList.append((0, 0, duration, 0, bpm))
+        elif ch == "+":
+            if text[i - 1] == "R":
+                octave += 1
+            elif text[i - 3 : i] == "BPM":
+                bpm += 80
+            else:
+                volume = min(127, 2 * volume)
+        elif ch == "-":
+            if text[i - 1] == "R":
+                octave -= 1
+            else:
+                volume = default_volume
+        elif ch == "?":
+            randomNote = "abcdefg"[random.randint(0, 6)]
+            notesList.append(
+                (notes[randomNote] + 12 * octave, volume, duration, instrument, bpm)
+            )
+        elif ch == "\n":
+            instrument = random.randint(0, 127)
+        elif ch == ";":
+            bpm = random.randint(1, 127)
+        else:
+            continue
+        time += 1
+    return notesList
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py [file-name]")
+    if len(sys.argv) != 3:
+        print("Usage: python %s [file-name] [output-name]" % sys.argv[0])
         exit(0)
 
     notesList = []
@@ -58,61 +109,4 @@ if __name__ == "__main__":
         print("File not found")
         exit(0)
 
-    prevChar = None
-
-    for i in range(len(text)):
-        ch = text[i].lower()
-        if not ch:
-            break
-
-        if ch in notes:
-            notesList.append(
-                (notes[ch] + 12 * octave, volume, duration, instrument, bpm)
-            )
-            prevChar = ch
-
-        elif ch in "oiu":
-            if prevChar in notes:
-                notesList.append(
-                    (notes[prevChar] + 12 * octave, volume, duration, instrument, bpm)
-                )
-                prevChar = ch
-            else:
-                notesList.append((notes["c"] + 12 * octave, volume, duration, 124, bpm))
-
-        elif ch == " ":
-            notesList.append((0, 0, duration, 0, bpm))
-
-        elif ch == "+":
-            if text[i - 1] == "R":
-                octave += 1
-            elif text[i - 3 : i] == "BPM":
-                bpm += 80
-            else:
-                volume = min(127, 2 * volume)
-
-        elif ch == "-":
-            if text[i - 1] == "R":
-                octave -= 1
-            else:
-                volume = default_volume
-
-        elif ch == "?":
-            randomNote = "abcdefg"[random.randint(0, 6)]
-            notesList.append(
-                (notes[randomNote] + 12 * octave, volume, duration, instrument, bpm)
-            )
-
-        elif ch == "\n":
-            instrument = random.randint(0, 127)
-
-        elif ch == ";":
-            bpm = random.randint(1, 127)
-
-        else:
-            continue
-
-        time += 1
-
-    print(notesList)
-    createMidi(notesList, "test.mid")
+    createMidi(translateText2MIDI(text), sys.argv[2].split(".")[0] + ".mid")
